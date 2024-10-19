bfc4e08cda6cc8d65f95608995cd74ba3efbdb55
Noah Beidelman <nbeidelman9@gmail.com>
2024-10-18 18:54:32 -0700

    change formatting, add test image

--- Inc/fonts.h
+++ Inc/fonts.h
@@ -3,12 +3,12 @@
 
 #include <stdint.h>
 
-typedef struct {
+typedef struct
+{
     const uint8_t width;
     uint8_t height;
     const uint16_t *data;
 } FontDef;
-
 
 extern FontDef Font_7x10;
 extern FontDef Font_11x18;
